5a271d436d35b6ddfbce684076f60c576dccdc89
RozninPotrick <Rozyfaki@gmail.com>
2024-06-13 13:35:35 +0100

    Add all files

---
+++ NewGitBDDProject/src/test/java/BrowserControl/CucumberHooks.java
@@ -0,0 +1,16 @@
+package BrowserControl;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class CucumberHooks extends  WebConnector{
+    @Before
+    public void setup(){
+        openBrowser();
+    }
+
+    @After
+    public void teardown(){
+        closeBrowser();
+    }
+}
---
+++ NewGitBDDProject/src/test/java/utilities/ConstantUtils.java
@@ -0,0 +1,12 @@
+package utilities;
+
+public class ConstantUtils {
+
+    public static String BASE_URL = "https://www.saucedemo.com/";
+
+    // Browser name chrome or Firefox
+    public static String BROWSER_NAME = "chrome";
+
+    // Browser type = Headed/Headless
+    public static String BROWSER_TYPE = "Headed";
+}
